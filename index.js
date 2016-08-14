64049bdbbaefb502f2d898d1bafd0951eee41412
Roger Staples <roger@whitehart.co.za>
2016-08-14 15:57:01 +0200

    Adding email verification

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ var api = new ParseServer({
 
 	//**** Email Verification ****//
 	/* Enable email verification */
-	// verifyUserEmails: true,
+	verifyUserEmails: true,
 	/* The public URL of your app */
 	// This will appear in the link that is used to verify email addresses and reset passwords.
 	/* Set the mount path as it is in serverURL */
@@ -36,14 +36,14 @@ var api = new ParseServer({
 	/* This will appear in the subject and body of the emails that are sent */
 	// appName: process.env.APP_NAME || "CodeCraft", 
 
-	// emailAdapter: {
-	// 	module: 'parse-server-simple-mailgun-adapter',
-	// 	options: {
-	// 		fromAddress: process.env.EMAIL_FROM || "test@example.com",
-	// 		domain: process.env.MAILGUN_DOMAIN || "example.com",
-	// 		apiKey: process.env.MAILGUN_API_KEY  || "apikey"
-	// 	}
-	// },
+	emailAdapter: {
+	 	module: 'parse-server-simple-mailgun-adapter',
+	 	options: {
+	 		fromAddress: process.env.EMAIL_FROM || "test@example.com",
+	 		domain: process.env.MAILGUN_DOMAIN || "example.com",
+	 		apiKey: process.env.MAILGUN_API_KEY  || "apikey"
+	 	}
+	},
 	
 	//**** File Storage ****//
 	// filesAdapter: new S3Adapter(
